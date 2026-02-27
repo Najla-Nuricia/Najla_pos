1e644c88b1a8ae2c7a508e801f3bd02afd639b43
Najla Nuricia Laudy <najlalaudy@gmail.com>
2026-02-27 15:53:50 +0700

    install adminlte_modularisasi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,29 @@
-<h1>Halaman Home</h1>
-<p>Selamat datang di website Najla</p>
+@extends('layouts.app');
+
+{{-- Customize layout section --}}
+
+@section('subtitle', 'Welcome')
+@section('content_header', 'Home')
+@section('content_header_subtitle', 'Welcome')
+
+{{-- Content body: main page content --}}
+
+@section('content_body')
+<p>Welcome to this beautiful admin panel</p>
+@stop
+
+{{-- Push extra CSS --}}
+
+@push('css')
+    {{-- Add here extra stylesheets --}}
+    {{--
+    <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@endpush
+
+{{-- Push extra scripts --}}
+
+@push('js')
+    <script>
+        console.log("Hi, I'm using the Laravel-AdminLTE package!");
+    </script>
+@endpush
