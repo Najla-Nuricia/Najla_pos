7fce488fa8e8d88b3ad029dfab3fc4c6bb960386
Najla Nuricia Laudy <najlalaudy@gmail.com>
2026-02-27 12:27:03 +0700

    relationship

---
+++ app/Models/LevelModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LevelModel extends Model
+{
+    protected $table = 'm_level';      // nama tabel
+    protected $primaryKey = 'level_id'; // primary key
+    public $timestamps = true;
+
+    protected $fillable = [
+        'level_nama'
+    ];
+}
